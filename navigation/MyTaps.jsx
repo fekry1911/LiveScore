69046b88d9a4302cc2f3dcb822d46f5b060404cb
Abdulrahman Fekry <abdofekryasda@gmail.com>
2025-10-25 20:21:34 +0300

    change readme

--- navigation/MyTaps.jsx
+++ navigation/MyTaps.jsx
@@ -1,6 +1,6 @@
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { Image } from 'react-native';
 import routes from '../utils/routes';
-import HomePage from '../pages/HomePage';
 import AllLeagues from '../components/AllLeagues';
 import AllMatches from '../components/AllMatches';
 
@@ -15,37 +15,44 @@ function MyTabs() {
           backgroundColor: '#1e293b',
           borderTopWidth: 0,
         },
-        sceneContainerStyle: {
-          backgroundColor: '#1e293b',
-        },
+        tabBarShowLabel: true,
+        tabBarActiveTintColor: '#fff',
+        tabBarInactiveTintColor: '#64748b',
       }}>
       <Tab.Screen
-        screenOptions={{
-          headerShown: false,
-          tabBarStyle: {
-            backgroundColor: '#1e293b',
-            borderTopWidth: 0,
-          },
-          sceneContainerStyle: {
-            backgroundColor: '#1e293b',
-          },
-        }}
         name={routes.home}
         component={AllMatches}
-      />
-      <Tab.Screen
-        screenOptions={{
-          headerShown: false,
-          tabBarStyle: {
-            backgroundColor: '#1e293b',
-            borderTopWidth: 0,
-          },
-          sceneContainerStyle: {
-            backgroundColor: '#1e293b',
-          },
+        options={{
+          tabBarLabel: 'Matches',
+          tabBarIcon: ({ focused }) => (
+            <Image
+              source={require('../assets/football.png')}
+              style={{
+                width: 24,
+                height: 24,
+              }}
+              resizeMode="contain"
+            />
+          ),
         }}
+      />
+
+      <Tab.Screen
         name={routes.leagues}
         component={AllLeagues}
+        options={{
+          tabBarLabel: 'Leagues',
+          tabBarIcon: ({ focused }) => (
+            <Image
+              source={require('../assets/league.png')}
+              style={{
+                width: 24,
+                height: 24,
+              }}
+              resizeMode="contain"
+            />
+          ),
+        }}
       />
     </Tab.Navigator>
   );
